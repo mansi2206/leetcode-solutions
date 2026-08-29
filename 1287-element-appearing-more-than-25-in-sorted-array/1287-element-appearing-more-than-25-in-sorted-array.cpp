e1855a5ce8ab9319beceac1ad01de869920ca7dd
Mansi Srivastava <142657614+mansi2206@users.noreply.github.com>
2026-08-29 23:01:12 +0530

    Time: 0 ms (100%), Space: 16.4 MB (58.01%) - LeetHub

--- 1287-element-appearing-more-than-25-in-sorted-array/1287-element-appearing-more-than-25-in-sorted-array.cpp
+++ 1287-element-appearing-more-than-25-in-sorted-array/1287-element-appearing-more-than-25-in-sorted-array.cpp
@@ -1,15 +1,32 @@
+// this is brute-force because the sorted array concept is not used anywhere.
+
+// class Solution {
+// public:
+//     int findSpecialInteger(vector<int>& arr) {
+//         unordered_map<int, int> cnt;
+//         int n = arr.size();
+//         for(int x: arr){
+//             cnt[x]++;
+//         }
+//         for(auto it: cnt){
+//             if(it.second >(n/4)){
+//                 return it.first;
+//             }
+//         }
+//         return -1;
+//     }
+// };
+
+// optimization
 class Solution {
 public:
     int findSpecialInteger(vector<int>& arr) {
-        unordered_map<int, int> cnt;
-        int n = arr.size();
-        for(int x: arr){
-            cnt[x]++;
-        }
-        // int p = n % 4;
-        for(auto it: cnt){
-            if(it.second >(n/4)){
-                return it.first;
+        int k = arr.size() / 4;
+
+        // i+k == i ye check krta h element repeat ho rha hain.
+        for (int i = 0; i + k < arr.size(); i++) {
+            if (arr[i] == arr[i + k]) {
+                return arr[i];
             }
         }
         return -1;
