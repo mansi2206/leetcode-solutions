18151e56d182b1a0eeb580e8fa7c91698c918996
Mansi Srivastava <142657614+mansi2206@users.noreply.github.com>
2026-09-06 17:54:04 +0530

    Time: 6 ms (14.02%), Space: 76.4 MB (67.85%) - LeetHub

--- 2656-maximum-sum-with-exactly-k-elements/2656-maximum-sum-with-exactly-k-elements.cpp
+++ 2656-maximum-sum-with-exactly-k-elements/2656-maximum-sum-with-exactly-k-elements.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
     int maximizeSum(vector<int>& nums, int k) {
-        int sum = 0;
 
-        while (k != 0) {
-            int maxi = INT_MIN;
-            int maxIdx = 0;
-            for (int i = 0; i < nums.size(); i++) {
-                if (nums[i] > maxi) {
-                    maxi = nums[i];
-                    maxIdx = i;
-                }
+        int sum = 0;
+        int maxi = INT_MIN;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] > maxi) {
+                maxi = nums[i];
             }
-            sum += maxi;
-            // replace that maxi number with maxi + 1 in the array
-            nums[maxIdx] = maxi + 1;
-            k--;
         }
+        // sum += maxi;
+        sum = k * (maxi + (maxi + k - 1)) / 2;
+        // while (k != 1) {
+        //     sum += maxi + 1;
+        //     maxi++;
+        //     k--;
+        // }
         return sum;
     }
 };
