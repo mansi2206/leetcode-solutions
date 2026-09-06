01674fd4f2c19c695f5b7ae294d565caac5760e0
Mansi Srivastava <142657614+mansi2206@users.noreply.github.com>
2026-09-06 22:22:28 +0530

    Time: 7 ms (65.62%), Space: 11.4 MB (54.83%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -27,10 +27,13 @@ public:
         }
 
         slow = head;
+        ListNode* prev = NULL;
         while (slow != fast) {
             slow = slow->next;
+            prev = fast;
             fast = fast->next;
         }
+        // prev -> next = NULL; // remove cycle
         return slow;
     }
 };
