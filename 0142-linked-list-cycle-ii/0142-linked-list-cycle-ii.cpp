4ececb74e23120ca0462b5c21ce83621b6ec1e4c
Mansi Srivastava <142657614+mansi2206@users.noreply.github.com>
2026-09-07 21:56:25 +0530

    Time: 11 ms (14.88%), Space: 11.3 MB (54.73%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -14,7 +14,7 @@ public:
         ListNode* fast = head;
         bool isCycle = false;
 
-        while (fast->next != NULL && fast != NULL) {
+        while (fast != NULL && fast->next != NULL) {
             slow = slow->next;
             fast = fast->next->next;
 
